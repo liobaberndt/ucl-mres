556e1dd512602aeacfe9217b03ba9ab53077cdaf
Sacha Popa <sachapopa@eduroam-int-dhcp-97-180-9.ucl.ac.uk>
2026-01-14 16:19:09 +0000

    Session 3 finished

--- module1/session3/script_04_exercise_variability.m
+++ module1/session3/script_04_exercise_variability.m
@@ -8,48 +8,55 @@
 % - Plot them for comparison (subplots or overlaid plots)
 
 % TODO: set sampling rate (Hz) and duration (s)
-fs = ;
-duration = ;
+fs = 250;
+duration = 2;
 
 % TODO: create time vector
-t = ;
+t = 0: 1/fs : duration;
 
 % TODO: choose frequency (Hz), amplitude, and noise strength
-frequency = ;
-amplitude = ;
-noise_strength = ;
+frequency = 5;
+amplitude = 1;
+noise_strength = 0.5;
 
 % TODO: create clean signal
-y_clean = ;
+y_clean = amplitude * sin(2 * pi * frequency * t);
 
 % TODO: create at least 3 noisy versions (each with randn)
-y_noisy_1 = ;
-y_noisy_2 = ;
-y_noisy_3 = ;
+y_noisy_1 = y_clean + (noise_strength * randn(size(t)));
+y_noisy_2 = y_clean + (noise_strength * randn(size(t)));
+y_noisy_3 = y_clean + (noise_strength * randn(size(t)));
+y_noisy_4 = y_clean + (noise_strength * randn(size(t)));
 
 % TODO: plot clean + noisy versions using subplots
 figure;
 
-subplot(4,1,1)
+subplot(5,1,1)
 plot(t, y_clean)
 xlabel('Time (s)')
 ylabel('Amplitude')
 title('Clean signal')
 
-subplot(4,1,2)
+subplot(5,1,2)
 plot(t, y_noisy_1)
 xlabel('Time (s)')
 ylabel('Amplitude')
 title('Noisy version 1')
 
-subplot(4,1,3)
+subplot(5,1,3)
 plot(t, y_noisy_2)
 xlabel('Time (s)')
 ylabel('Amplitude')
 title('Noisy version 2')
 
-subplot(4,1,4)
+subplot(5,1,4)
 plot(t, y_noisy_3)
 xlabel('Time (s)')
 ylabel('Amplitude')
 title('Noisy version 3')
+
+subplot (5,1,5)
+plot(t, y_noisy_4)
+xlabel('Time (s)')
+ylabel ('Amplitude')
+title('Noisy version 4')
