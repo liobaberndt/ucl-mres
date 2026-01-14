6570803ad98ec02efc3e93ce5efb81b5c7db6031
Sacha Popa <sachapopa@eduroam-int-dhcp-97-180-9.ucl.ac.uk>
2026-01-14 16:19:09 +0000

    Session 3 finished

--- module1/session3/script_02_exercise_noise_level.m
+++ module1/session3/script_02_exercise_noise_level.m
@@ -8,30 +8,30 @@
 % - Plot clean, low-noise, and high-noise signals for comparison
 
 % TODO: set sampling rate (Hz) and duration (s)
-fs = ;
-duration = ;
+fs = 250;
+duration = 2;
 
 % TODO: create a time vector
-t = ;
+t = 0: 1/fs : duration;
 
 % TODO: choose frequency (Hz) and amplitude
-frequency = ;
-amplitude = ;
+frequency = 10;
+amplitude = 3;
 
 % TODO: create the clean signal
-y_clean = ;
+y_clean = amplitude * sin(2*pi*frequency*t);
 
 % TODO: choose two different noise strengths
-noise_strength_low = ;
-noise_strength_high = ;
+noise_strength_low = 0.25;
+noise_strength_high = 100;
 
 % TODO: create two independent noise vectors (use randn)
-noise_low = ;
-noise_high = ;
+noise_low = noise_strength_low * randn(size(t));
+noise_high = noise_strength_high * randn(size(t));
 
 % TODO: create the noisy signals
-y_low_noise = ;
-y_high_noise = ;
+y_low_noise = y_clean + noise_low;
+y_high_noise = y_clean + noise_high;
 
 % TODO: plot all three signals using subplots
 figure;
